5e6be0aff159573c0314d31f5fc6987cea64ed8f
Joao Paulo <preti.joao@ifmt.edu.br>
2023-02-23 19:26:49 -0400

    fix: exercicios2

--- exercicios02.c
+++ exercicios02.c
@@ -7,42 +7,81 @@ EXERCÍCIOS DO CAPÍTULO 2
 #include<math.h>
 
 //1. Faça um programa que leia dois valores numéricos inteiros e efetue a adição, caso o resultado seja maior que 10, apresentá-lo.
+void questao01() {
+	
+}
 
 //2. Faça um programa que leia dois valores inteiros e efetue a adição. Caso o valor somado seja maior que 20, este deverá ser apresentado somando-se a ele mais 8, caso o valor somado seja menor ou igual a 20, este deverá ser apresentado subtraindo-se 5.
+void questao02() {
+	
+}
 
 //3. Faça um programa que leia um número e imprima uma das duas mensagens: "É múltiplo de 3"ou "Não é múltiplo de 3".
+void questao03() {
+	
+}
 
 //4. Faça um programa que leia um número e informe se ele é ou não divisível por 5.
+void questao04() {
+	
+}
 
 //5. Faça um programa que leia um número e informe se ele é divisível por 3 e por 7.
+void questao05() {
+	
+}
 
 //6. A prefeitura do Rio de Janeiro abriu uma linha de crédito para os funcionários
 //estatutários. O valor máximo da prestação não poderá ultrapassar 30% do salário
 //bruto. Faça um programa em linguagem C que permita entrar com o salário bruto e o
 //valor da prestação e informar se o empréstimo pode ou não ser concedido.
+void questao06() {
+	
+}
 
 //7. Faça um programa que leia um número e indique se o número está compreendido
 //entre 20 e 50 ou não.
+void questao07() {
+	
+}
 
 //8. Faça um programa que leia um número e imprima uma das mensagens: 
 //"Maior do que 20", "Igual a 20"ou "Menor do que 20".
+void questao08() {
+	
+}
 
 //9. Faça um programa que permita entrar com o ano de nascimento da pessoa e como ano
 //atual. O programa deve imprimir a idade da pessoa. Não se esqueça de verificar se o
 //ano de nascimento informado é válido.
+void questao09() {
+	
+}
 
 //10. Faça um programa que leia três números inteiros e imprima os três em ordem
 //crescente.
+void questao10() {
+	
+}
 
 //11. Faça um programa que leia 3 números e imprima o maior deles.
+void questao11() {
+	
+}
 
 //12. Faça um programa que leia a idade de uma pessoa e informe:
 //• Se é maior de idade
 //• Se é menor de idade
 //• Se é maior de 65 anos
+void questao12() {
+	
+}
 
 //13. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota da
 //prova 2 de umaluno. O programa deve imprimir o nome, a nota da prova 1, a nota da prova 2, a média das notas e uma das mensagens: "Aprovado", "Reprovado"ou "em Prova Final"(a média é 7 para aprovação, menor que 3 para reprovação e as demais em prova final).
+void questao13() {
+	
+}
 
 //14. Faça um programa que permita entrar com o salário de uma pessoa e imprima o
 //desconto do INSS segundo a tabela seguir:
@@ -51,9 +90,15 @@ EXERCÍCIOS DO CAPÍTULO 2
 //Maior que R$600,00 e menor ou igual a R$1200,00 20%
 //Maior que R$1200,00 e menor ou igual a R$2000,00 25%
 //Maior que R$2000,00 30%
+void questao14() {
+	
+}
 
 //15. Um comerciante comprou umproduto e quer vendê-lo com umlucro de 45% se o valor
 //da compra for menor que R$20,00, caso contrário, o lucro será de 30%. Faça um programa que leia o valor do produto e imprima o valor da venda.
+void questao15() {
+	
+}
 
 //16. A confederação brasileira de natação irá promover eliminatórias para o próximo
 //mundial. Faça um programa que receba a idade de um nadador e imprima a sua categoria segundo a tabela a seguir:
@@ -63,6 +108,9 @@ EXERCÍCIOS DO CAPÍTULO 2
 //Juvenil A 11 - 13 anos
 //Juvenil B 14 - 17 anos
 //Sênior maiores de 18 anos
+void questao16() {
+	
+}
 
 //17. Depois da liberação do governo para as mensalidades dos planos de saúde, as pessoas começaram a fazer pesquisas para descobrir um bom plano, não muito caro. Umvendedor de um plano de saúde apresentou a tabela a seguir. Faça um programa que entre com o nome e a idade de uma pessoa e imprima o nome e o valor que ela deverá pagar.
 //Idade Valor
@@ -72,13 +120,22 @@ EXERCÍCIOS DO CAPÍTULO 2
 //Acima de 45 até 59 anos R$150,00
 //Acima de 59 até 65 anos R$250,00
 //Maior que 65 anos R$400,00
+void questao17() {
+	
+}
 
 //18. Faça um programa que leia um número inteiro entre 1 e 12 e escreva o mês correspondente. Caso o usuário digite umnúmero fora desse intervalo, deverá aparecer uma mensagem informando que não existe mês com este número. Utilize o switch para este problema.
+void questao18() {
+	
+}
 
 //19. Em um campeonato nacional de arco-e-flecha, tem-se equipes de três jogadores para cada estado. Sabendo-se que os arqueiros de uma equipe não obtiveram o mesmo número de pontos, criar um programa que informe se uma equipe foi classificada, de acordo com a seguinte especificação:
 //• Ler os pontos obtidos por cada jogador da equipe;
 //• Mostrar esses valores em ordem decrescente;
 //• Se a soma dos pontos for maior do que 100, imprimir a média aritmética entre eles, caso contrário, imprimir a mensagem "Equipe desclassificada".
+void questao19() {
+	
+}
 
 //20. O banco XXX concederá um crédito especial com juros de 2% aos seus clientes de acordo com o saldomédio no último ano. Faça um programa que leia o saldo médio de um cliente e calcule o valor do crédito de acordo com a tabela a seguir. O programa deve imprimir uma mensagem informando o saldo médio e o valor de crédito.
 //Saldo Médio Percentual
@@ -86,13 +143,22 @@ EXERCÍCIOS DO CAPÍTULO 2
 //de 501 a 1000 30% do valor do saldo médio
 //de 1001 a 3000 40% do valor do saldo médio
 //acima de 3001 50% do valor do saldo médio
+void questao20() {
+	
+}
 
 //21. A biblioteca de uma Universidade deseja fazer um programa que leia o nome do livro que será emprestado, o tipo de usuário (professor ou aluno) e possa imprimir um recibo conforme mostrado a seguir. Considerar que o professor tem dez dias para devolver o livro e o aluno só três dias.
 //• Nome do livro:
 //• Tipo de usuário:
 //• Total de dias:
+void questao21() {
+	
+}
 
 //22. Construa um programa que leia o percurso em quilómetros, o tipo do carro e informe o consumo estimado de combustível, sabendo-se que umcarro tipo C faz 12 kmcom umlitro de gasolina, um tipo B faz 9 kme o tipo C, 8 kmpor litro.
+void questao22() {
+	
+}
 
 //23. Crie um programa que informe a quantidade total de calorias de uma refeição a partir da escolha do usuário que deverá informar o prato, a sobremesa, e bebida conforme a tabela a seguir.
 //Prato Sobremesa Bebida
@@ -100,16 +166,50 @@ EXERCÍCIOS DO CAPÍTULO 2
 //Peixe 230cal Sorvete diet 110cal Suco de laranja 70cal
 //Frango 250cal Mousse diet 170cal Suco de melão 100cal
 //Carne 350cal Mousse chocolate 200cal Refrigerante diet 65cal
+void questao23() {
+	
+}
 
 //24. A polícia rodoviária resolveu fazer cumprir a lei e vistoriar veículos para cobrar dosmotoristas o DUT. Sabendo-se que o mês em que o emplacamento do carro deve ser renovado é determinado pelo último número da placa do mesmo, faça um programa que, a partir da leitura da placa do carro, informe omês em que o emplacamento deve ser renovado.
+void questao24() {
+	
+}
 
 //25. A prefeitura contratou uma firma especializada para manter os níveis de poluição considerados ideais para umpaís do 1º mundo. As indústrias, maiores responsáveis pela poluição, foram classificadas em três grupos. Sabendo-se que a escala utilizada varia de 0,05 e que o índice de poluição aceitável é até 0,25, fazer um programa que possa imprimir intimações de acordo com o índice e a tabela a seguir:
 //Índice Indústrias que receberão intimação
 //0,3 1º gurpo
 //0,4 1º e 2º grupos
 //0,5 1º, 2º e 3º grupos
+void questao25() {
+	
+}
 
 int main() {
-
+	questao01();
+	questao02();
+	questao03();
+	questao04();
+	questao05();
+	questao06();
+	questao07();
+	questao08();
+	questao09();
+	questao10();
+	questao11();
+	questao12();
+	questao13();
+	questao14();
+	questao15();
+	questao16();
+	questao17();
+	questao18();
+	questao19();
+	questao20();
+	questao21();
+	questao22();
+	questao23();
+	questao24();
+	questao25();
+	
 	return EXIT_SUCCESS;
 }
